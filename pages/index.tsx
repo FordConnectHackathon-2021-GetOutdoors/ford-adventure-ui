6d0f3e8e1ac83a94ef3af9cdb0472ef258c784e9
Matt Wood <mattwooddc@gmail.com>
2021-06-19 15:27:27 -0400

    fix build again

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,14 @@
-import { Container, Box } from "@chakra-ui/react"
-import Head from 'next/head'
-import { appTitle } from "../next.config"
-import Landing from "../components/Landing/Landing"
+import { Container, Box } from "@chakra-ui/react";
+import Landing from "../components/Landing/Landing";
 
 export default function Index() {
   return (
     <div>
-      <Head>
-        <title>{ appTitle }</title>
-      </Head>
       <Container>
         <Box>
           <Landing />
         </Box>
       </Container>
     </div>
-  )
+  );
 }
