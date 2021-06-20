3edb7c3926f690d5adb8fc495acca877c9ab4775
Shrikant Kakaraparthi <shrikant.kakaraparthi@outlook.com>
2021-06-20 18:53:05 -0500

    test text

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ function AppContainer({ Component, pageProps }: AppProps) {
     <ThemeProvider cookies={pageProps.cookies}>
       <Auth.UserContextProvider supabaseClient={supabase}>
         <CustomHead {...pageProps} />
+          <h1>hi</h1>
         <Component {...pageProps} />
         <ThemeSwitcher />
       </Auth.UserContextProvider>
