069877fc55d70a035056cae3c78266acde656207
Matt Wood <mattwooddc@gmail.com>
2021-07-08 23:13:21 -0400

    add bottom button

--- pages/go/[adventure].tsx
+++ pages/go/[adventure].tsx
@@ -224,6 +224,26 @@ export default function Adventure({
             );
           })}
         </Flex>
+        <Button
+          bg="transparent"
+          p="5"
+          pos="fixed"
+          bottom="0"
+          zIndex="4"
+          w="100%"
+        >
+          <Box
+            bg="bg.darknavy"
+            color="white"
+            fontFamily="FontAntenna"
+            px="4"
+            py="4"
+            w="20rem"
+            borderRadius="md"
+          >
+            Let's Go!
+          </Box>
+        </Button>
         <Box bg="white" px={10} pt={5} flexGrow={1} overflow="scroll">
           <AnimatePresence>
             {adventureTabs.map((tab: any, idx: number) => {
