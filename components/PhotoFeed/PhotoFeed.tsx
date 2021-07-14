3f1aaa7b552aa2911127885ddb93f339c41416f0
Matt Wood <mattwooddc@gmail.com>
2021-07-13 21:46:01 -0400

    update

--- components/PhotoFeed/PhotoFeed.tsx
+++ components/PhotoFeed/PhotoFeed.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { useContext, useState } from "react";
 import { PhotoPost } from "components/PhotoPost/PhotoPost";
 import { useMemo } from "react";
@@ -5,41 +6,40 @@ import { AuthContext } from "utils/AuthContext";
 import { supabase } from "utils/supabase";
 
 export function PhotoFeed() {
-    const { user } = useContext(AuthContext);
-    const [posts, setPosts] = useState([]);
-    const [userDetails, setUserDetails] = useState();
-    const userUploadsPromise = useMemo(async () => {
-        const user = supabase.auth.user();
-        setUserDetails(user);
-        console.log(user)
-        return await supabase
-                .from('user_uploads')
-                .select('*')
-                .eq('user_id', "7a26a2d3-b8bc-44d5-a7ec-8cda57ff8c23")
-                .eq('active', true);
-    }, []);
+  const { user } = useContext(AuthContext);
+  const [posts, setPosts] = useState([]);
+  const [userDetails, setUserDetails] = useState();
+  const userUploadsPromise = useMemo(async () => {
+    const user = supabase.auth.user();
+    setUserDetails(user);
+    console.log(user);
+    return await supabase
+      .from("user_uploads")
+      .select("*")
+      .eq("user_id", "7a26a2d3-b8bc-44d5-a7ec-8cda57ff8c23")
+      .eq("active", true);
+  }, []);
 
-    userUploadsPromise.then(({ data }) => {
-        if (!!data) {
-            setPosts(data);
-        }
-    });
+  userUploadsPromise.then(({ data }) => {
+    if (!!data) {
+      setPosts(data);
+    }
+  });
 
-    
-    return (
-        <>
-            {
-                posts && posts.length > 0 &&
-                posts.map((upload_data: any) => (
-                    <PhotoPost 
-                        key={Math.random()} 
-                        imgSrc={upload_data['image_base64']} 
-                        username={userDetails['email']}
-                        vehicleName={'F-150'}
-                        created={upload_data['created']}
-                        content={upload_data['content']} />
-                ))
-            }
-        </>
-    );
+  return (
+    <>
+      {posts &&
+        posts.length > 0 &&
+        posts.map((upload_data: any) => (
+          <PhotoPost
+            key={Math.random()}
+            imgSrc={upload_data["image_base64"]}
+            username={userDetails["email"]}
+            vehicleName={"F-150"}
+            created={upload_data["created"]}
+            content={upload_data["content"]}
+          />
+        ))}
+    </>
+  );
 }
