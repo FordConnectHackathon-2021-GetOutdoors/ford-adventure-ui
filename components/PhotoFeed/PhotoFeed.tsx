5495011aaa6e830b0623083171c3169182d9d788
Matt Wood <mattwooddc@gmail.com>
2021-07-14 01:14:40 -0400

    fix photo feed

--- components/PhotoFeed/PhotoFeed.tsx
+++ components/PhotoFeed/PhotoFeed.tsx
@@ -1,33 +1,28 @@
-// @ts-nocheck
 import React, { useContext, useState } from "react";
 import { PhotoPost } from "components/PhotoPost/PhotoPost";
-import { useMemo } from "react";
-import { AuthContext } from "utils/AuthContext";
+// import { useMemo } from "react";
+// import { AuthContext } from "utils/AuthContext";
 import { supabase } from "utils/supabase";
+import { useEffect } from "react";
 
 export function PhotoFeed() {
   // const { user } = useContext(AuthContext);
   const [posts, setPosts] = useState([]);
-  const [userDetails, setUserDetails] = useState();
-  const userUploadsPromise = useMemo(async () => {
-    const user = supabase.auth.user();
+
+  const [userDetails, setUserDetails] = useState<any>();
+  const user = supabase.auth.user();
+
+  useEffect(() => {
     if (user) {
       setUserDetails(user);
-      console.log(user);
-      return await supabase
+      supabase
         .from("user_uploads")
         .select("*")
         .eq("user_id", "7a26a2d3-b8bc-44d5-a7ec-8cda57ff8c23")
-        .eq("active", true);
+        .eq("active", true)
+        .then(({ data }: any) => !posts.length && data && setPosts(data));
     }
-  }, []);
-
-  userUploadsPromise &&
-    userUploadsPromise.then(({ data }) => {
-      if (!!data) {
-        setPosts(data);
-      }
-    });
+  }, [user]);
 
   if (!posts) {
     return null;
