38cfcb23a6d5c7428379c16b99db8d78088103eb
Matt Wood <mattwooddc@gmail.com>
2021-07-14 20:35:57 -0400

    update deps

--- orval/vehicle-information/vehicle-information.msw.ts
+++ orval/vehicle-information/vehicle-information.msw.ts
@@ -4,19 +4,16 @@
  * FordConnect Prod Collection V3.1.2
  * OpenAPI spec version: 1.0.0
  */
-import {
-  rest
-} from 'msw'
+import { rest } from "msw";
 
 export const getVehicleInformationMSW = () => [
-rest.post('*/api/fordconnect/vehicles/v1/:vehicleId/status', (req, res, ctx) => {
-        return res(
-          ctx.delay(1000),
-          ctx.status(200, 'Mocked status'),
-        )
-      }),rest.get('*/api/fordconnect/vehicles/v1/:vehicleId', (req, res, ctx) => {
-        return res(
-          ctx.delay(1000),
-          ctx.status(200, 'Mocked status'),
-        )
-      }),]
+  rest.post(
+    "*/api/fordconnect/vehicles/v1/:vehicleId/status",
+    (req, res, ctx) => {
+      return res(ctx.delay(1000), ctx.status(200, "Mocked status"));
+    }
+  ),
+  rest.get("*/api/fordconnect/vehicles/v1/:vehicleId", (req, res, ctx) => {
+    return res(ctx.delay(1000), ctx.status(200, "Mocked status"));
+  }),
+];
